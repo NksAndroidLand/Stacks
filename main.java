8f4ed18c9a526d5286b625d365f8964bd4ba2ca2
Ramokgopa <freddyrams@gmail.com>
2020-02-11 14:42:48 +0200

    Fixed Identation and changed naming convention

--- main.java
+++ main.java
@@ -46,59 +46,59 @@ public class main {
 			}
 			//Converting data for Programmatic stack to json
 			Document doc = Jsoup.parse(sb.toString());//parsing the string into the Jsoup library to be read as html 
-				JSONArray jArray = new JSONArray();//creating a jsonArray to store each json Object
-			    Element table = doc.select("tbody").first();//getting the contents of the table to be stored in the json Object
-			    Elements row = table.getElementsByTag("tr");       
-			        for (int r = 0, c = row.size(); r < c; r++) {
-			        	JSONObject jsonObject = new JSONObject();
-			        	Elements cols = row.get(r).select("td");
-			            String Tech = cols.get(0).text();
-			            String Reason = cols.get(1).text();
-			            String Lifecycle = cols.get(2).text();
-			            jsonObject.put("Tech", Tech);
-			            jsonObject.put("Reason", Reason);
-			            jsonObject.put("Lifecycle", Lifecycle);
-			            jArray.put(jsonObject);
-			        }
-			      //Converting data for Build stack to json    
-			        JSONArray jArray1 = new JSONArray();
-				    Element table1 = doc.select("tbody").get(1);
-				    Elements row1 = table1.getElementsByTag("tr");       
-				        for (int r = 0, c = row1.size(); r < c; r++) {
-				        	JSONObject jsonObject = new JSONObject();
-				        	Elements cols = row1.get(r).select("td");
-				            String Tech = cols.get(0).text();
-				            String Use = cols.get(1).text();
-				            String Reason = cols.get(2).text();
-				            String Lifecycle = cols.get(3).text();
-				            jsonObject.put("Tech", Tech);
-				            jsonObject.put("Use", Use);
-				            jsonObject.put("Reason", Reason);
-				            jsonObject.put("Lifecycle", Lifecycle);
-				            jArray1.put(jsonObject);
-				        }
-				   //Converting data for Infrastructure to json
-				        JSONArray jArray2 = new JSONArray();
-					    Element table2 = doc.select("tbody").get(1);
-					    Elements row2 = table1.getElementsByTag("tr");       
-					        for (int r = 0, c = row2.size(); r < c; r++) {
-					        	JSONObject jsonObject = new JSONObject();
-					        	Elements cols = row2.get(r).select("td");
-					            String Tech = cols.get(0).text();
-					            String Use = cols.get(1).text();
-					            String Empty = cols.get(2).text();
-					            jsonObject.put("Tech", Tech);
-					            jsonObject.put("Use", Use);
-					            jsonObject.put(" ", Empty);
-					            jArray2.put(jsonObject);
-					        }
+			JSONArray jarrHtml = new JSONArray();//creating a jsonArray to store each json Object
+			Element etable = doc.select("tbody").first();//getting the contents of the table to be stored in the json Object
+			Elements erow = etable.getElementsByTag("tr");       
+			   for (int r = 0; r <erow.size(); r++) {
+			        JSONObject jsoRecord = new JSONObject();
+			        Elements ecols = erow.get(r).select("td");
+			        String strTech = ecols.get(0).text();
+			        String strReason = ecols.get(1).text();
+			        String strLifecycle = ecols.get(2).text();
+			        jsoRecord.put("Tech", strTech);
+			        jsoRecord.put("Reason", strReason);
+			        jsoRecord.put("Lifecycle", strLifecycle);
+			        jarrHtml.put(jsoRecord);
+		        }
+			//Converting data for Build stack to json    
+		    JSONArray jarrHtml1 = new JSONArray();
+			Element etable1 = doc.select("tbody").get(1);
+		    Elements erow1 = etable1.getElementsByTag("tr");       
+				for (int r = 0;r < erow1.size(); r++) {
+				   	JSONObject jsoRecord = new JSONObject();
+			     	Elements ecols = erow1.get(r).select("td");
+		            String Tech = ecols.get(0).text();
+		            String Use = ecols.get(1).text();
+			        String Reason = ecols.get(2).text();
+		            String Lifecycle = ecols.get(3).text();
+		            jsoRecord.put("Tech", Tech);
+		            jsoRecord.put("Use", Use);
+		            jsoRecord.put("Reason", Reason);
+		            jsoRecord.put("Lifecycle", Lifecycle);
+		            jarrHtml1.put(jsoRecord);
+	        }
+			//Converting data for Infrastructure to json
+			JSONArray jarrHtml2 = new JSONArray();
+		    Element etable2 = doc.select("tbody").get(1);
+		    Elements erow2 = etable1.getElementsByTag("tr");       
+		        for (int r = 0; r < erow2.size(); r++) {
+		            JSONObject jsoRecord = new JSONObject();
+		        	Elements ecols = erow2.get(r).select("td");
+		        	String Tech = ecols.get(0).text();
+		            String Use = ecols.get(1).text();
+		            String Empty = ecols.get(2).text();
+		            jsoRecord.put("Tech", Tech);
+		            jsoRecord.put("Use", Use);
+		            jsoRecord.put(" ", Empty);
+		            jarrHtml2.put(jsoRecord);
+	        }
 					    
 				  //Outputting resultant jsonArray
-			        JSONObject stackjson = new JSONObject(); 
-			        stackjson.put("Programmatic stack", jArray);
-			        stackjson.put("Build Stack", jArray1);
-			        stackjson.put("Infrastructure", jArray2);
-			        System.out.println(stackjson.toString().replace(",", "\n,"));
+		     JSONObject stackjson = new JSONObject(); 
+	         stackjson.put("Programmatic stack", jarrHtml);
+	         stackjson.put("Build Stack", jarrHtml1);
+	         stackjson.put("Infrastructure", jarrHtml2);
+	         System.out.println(stackjson.toString().replace(",", "\n,"));
 			    
 			
 		
